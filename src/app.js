0e9191d1e68e2d579b9b8ac57b6d5c17d4c8f8b1
Appukumaryadav001 <appukumaryadav19@gmail.com>
2026-03-28 10:29:48 +0530

    initial project setup

--- src/app.js
+++ src/app.js
@@ -1,6 +1,24 @@
-import express from "express";
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+
+import authRoutes from './src/routes/auth.routes.js';
+import taskRoutes from './src/routes/task.routes.js';
+import errorHandler from './src/middleware/errorHandler.middleware.js';
+
 const app = express();
-app.get("/", (req,res)=>{
-  res.send("appu")
-})
-export {app};
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/tasks', taskRoutes);
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Todo App API is running!' });
+});
+
+app.use(errorHandler);
+
+export { app };
